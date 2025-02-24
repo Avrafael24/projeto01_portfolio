808bea9ba9e043eeb128795de3505cf1f30507ff
Avrafael24 <devrafael04@gmail.com>
2025-02-24 04:58:18 +0000

    Add files via upload

---
+++ PAGINAS/TEXTOS.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const text = `
+    O Eu representa um conjunto de modelos naturais que nos torna singular, o desenvolvimento do eu surge como consequência da imersão direta que temos com um meio existencial e cada meio existencial apresenta diferentes linhagens de pensamentos certamente, e essa linhagem de pensamento faz brotar padrões comportamentais que vão acabar por definir o tipo de meio. Os nossos princípios e crenças são os maiores responsáveis pela definição da nossa maneira de Pensar-Ser-Viver ou simplesmente PSV.
+    
+    O nosso PSV é um simples espelho que reflete a cultura educacional que acarretamos, e essa mesma cultura educacional, representa a base ou o pilar do nosso Eu, a qualidade dessa cultura educacional desenrola um papel fundamental na definição da integridade e do carácter do eu. Andamos em ambiente altamente flutuante, onde as influências vagueiam a quase todo espaço que nos encontramos, por essa razão nós somos escravos contínuos das influências, é quase impossível estarmos livre dela, porque onde há socialização sempre há de existir influências. As influências desta feita, têm sido os grandes responsáveis na mudança dos modelos que compõem os nossos princípios e crenças, ela é um fator que pode arrancar o Eu da sua "realidade original" e o leva até a uma "realidade comparativa" uma realidade onde o Eu vive a base comparações, onde o Eu procura igualdade na desigualdade, Onde o Eu é obrigado a sacrificar os seus princípios e integridade simplesmente para ser ele ou ela, e tudo isso representa uma grande traição para com a natureza, pois segundo a natureza, o que define um ser, é a sua singularidade e não a sua pluralidade.
+    
+    O meio externo ou existencial sempre foi um dos maiores responsáveis pela definição do EU, pelo facto de que todo meio carrega um grau acentuado de influências, cada EU quando exposto em um meio deve obrigatoriamente adaptar-se as condições impostas pelo meio, afim de poder ser aceite por esse mesmo meio, e cada meio como já tivera dito antes, apresenta um modelo de pensamento, que com base nesse modelo de pensamento, o meio vai possuir uma forma de viver, o grande desafio entra em jogo, pois é a partir desse momento que surgem "indivíduos esponjas" e "indivíduos peneira", os indivíduos esponjas são aqueles que absorvem tudo quanto existe no meio sem fazer uma avaliação integral daquilo que está absorver, são indivíduos que facilmente deixam-se levar pelas energias incorretas do meio, e isso tudo acontece porque, os seus princípios e a sua essência são facilmente corrompidas, e no outro lado temos os indivíduos peneira, que agem de maneira contrária, são aqueles que filtram tudo aquilo quanto é benéfico para o seu Eu, são indivíduos que mantêm firme os seus princípios, ou seja, são indivíduos que agem como garrafa térmicas, mantém constante o seu conteúdo interno, evitando uma troca de calor com meio externo, pois eles tão consciente que essa troca, pode causar alterações no conteúdo interno.
+    
+    Desta feita, a desconexão do Eu surge por meio dessa perspectiva abordada: quando deixamos de viver com base os nossos princípios e crenças e passamos a viver com base nos princípios e crenças de outrem, não podemos permitir que as influências nos levem até a realidade comparativa, pois é aí onde a gravidade da situação ganha poder, ficamos tão preso a essa realidade comparativa e esquecemos da realidade que nos torna singular, a realidade que nos torna singular apenas quer tornar-nos melhor do que nós mesmo, quer fazer de nós o nosso próprio espelho, criando assim maior grau de satisfação, já a realidade comparativa quer apenas te tornar melhor ou igual a outra pessoa, quando na verdade, pessoas nunca serão melhores do que pessoas, e pessoas nunca são iguais a pessoas, cada um apresenta aptidões e características específicas que vão definir a unicidade do Ser❣️.
+    `;
+    document.getElementById('justified-text').innerHTML = text;
+  });
+
+
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const text = `
+    Verdadeiramente constatamos que todo ser humano residente nos demais pontos do universo, é dotado de inteligência, a inteligência tem sido o agente importante na definição e configuração da vida do homem, graças a mesma o homem é capaz de realizar as diversas operações encontradas ao longo da sua vida e adaptar-se aos diversos contextos instalados no mundo contemporâneo.
+
+    O mundo na sua integridade é um local abarrotado com milhares de informações, o homem como elemento  desse ecossistema , deve indispensavelmente acompanhar o dinamismo envolvente a sua volta, a fim de criar um ajustamento e estabilidade em seu trajeto de vida; todo trajeto humano é continuamente cercado de situações desafiadoras, essa carga de  desafios contínuos, representa simplesmente  a uns dos verdadeiros sentidos do jogo da vida ; a inteligência humana é um sistema projetado para então auxiliar o homem na manipulação das diferentes situações encontradas em seu percurso de vida.
+
+    O desenvolvimento das sociedades nos demais sectores, econômico, social e tecnológico, é fruto da expansão a nível intelectual do homo-sapiens, graças a essa magnitude de conhecimento, o homem tem sido cada vez mais capaz de simplificar e calibrar a vida em sociedade. O conhecimento por natureza é um meio dependente da inteligência, é por intermédio da mesma que o homem é capaz de construir os pilares que foram a base estrutural do próprio conhecimento.
+
+    Toda sociedade por defeito apresenta diversas estruturas sociais, como a Família, escola , igreja  e etc. As estruturas sociais servem como meios facilitadores no processo de estimulação do conhecimento e da inteligência humana, a estrutura social "família", é um meio padrão, é a partir dela que o ser humano começa a ganhar os princípios básicos de filosofia e política de vida, com esses princípios básicos, o ser humano torna-se apto para lidar com as demais estruturas sociais.
+
+    A estrutura social "escola" intuitivamente existe com  objetivo claro de desenvolver as capacidades intelectuais de seus educandos por meio da partilha de conhecimento académico, que por sua vez se encontra distribuído em níveis, o sistema de ensino moderno tem afetado bastante a carreira de seus educandos pelo fato de não respeitar devidamente com o princípio da "unicidade intelectual" , esse princípio resumidamente afirma que "cada ser humano apresenta aptidões e características intelectuais que os torna único", logo a inteligência humana é um elemento multifacetado ou ramificado em "tipos" como exemplo, a inteligência lógico-matemática, indivíduos predominante nesse tipo, lidam facilmente com números, padrões e com situações mais racionais de um modo geral, enquanto que indivíduos com uma inteligência linguística aguçada desdobram-se facilmente em aprendizado de idiomas por exemplo; portanto, essa ramificação em "tipos" da inteligência pode justificar a razão de certos indivíduos serem rápidos e ágeis em certas áreas mas lentos em outras.
+
+    O sistema educacional falha no sentido de que o mesmo não consegue produzir mecanismos que possibilitam exercer nitidamente o princípio da "unicidade intelectual", o mesmo é encapsulado de atividades que tentam adaptar todos os seus "educandos " em um único regime, regime esse que não se classifica como um procedimento concreto para explorar devidamente cada educando, esse facto faz com que todo aquele potencial camuflado em cada discente seja negligenciado; com essas práticas, o discente facilmente molda a sua realidade colocando-se em uma situação no qual o mesmo sente-se inábil de demonstrar o seu universo criativo e culto, essa incapacidade de expressão é que geralmente fundamenta  o motivo de diversos discentes adotarem uma postura mais passiva e indolente quanto ao aprendizado na escola, cada vez que o educando é submetido a "testes" mais o seu subconsciente vai reforçando a ideia de que é "incapaz", isso porque os "testes ou provas" são práticas furadas e enganosas que tentam definir o quanto um indivíduo tem domínio de uma informação, e geralmente esse grau de conhecimento é associado a um número na escala de 0-20, essa escala tem sido por muito tempo a referência para definir até que ponto um indivíduo é capaz, e é também através dessa mesma escala onde entram as classificações tendenciosas de mais e menos inteligente, o educando no meio desse ambiente classificativo e tendencioso acaba por duvidar das suas próprias competências e podendo criar pensamentos ilusórios que visualizam aquele simples "número" em folha de papel com sendo algo que irá definir a sua essência.
+
+    O pacote da essência humana já contém a inteligência como um fator inato e mutável, mesmo com um rolo contínuo de adversidades, ela sempre existirá, nenhum sistema na natureza ainda está apto para definir a inteligência de cada indivíduo, pois a inteligência é um elemento imensurável, é um elemento que transcende os horizontes do universo intelectual, ela vai muito mais além daquilo que a escola por exemplo, "diz" explorar , todo ser humano é integramente inteligente e capaz, o que falta apenas são procedimentos certos e adequados para transcendermos e explorarmos por completo todo aquele potencial e energia intelectual dentro dos mistérios da mente 🔮.
+
+
+
+    `;
+    document.getElementById('justified-text_1').innerHTML = text;
+  });
+
+
+
+  // Função para adicionar classe ativa aos cartões visíveis
+function addActiveClass() {
+    var cards = document.querySelectorAll('.card');
+    cards.forEach(function(card) {
+      var cardPosition = card.getBoundingClientRect().top;
+      var screenPosition = window.innerHeight / 1.3; // Ajuste conforme necessário
+      if (cardPosition < screenPosition) {
+        card.classList.add('active');
+      } else {
+        card.classList.remove('active');
+      }
+    });
+  }
+  
+  // Função de controle de evento de rolagem
+  window.addEventListener('scroll', addActiveClass);
+  
+  // Chame a função para garantir que os cartões visíveis sejam marcados inicialmente
+  addActiveClass();
+  
+  
+  
+
+
+
+
+
